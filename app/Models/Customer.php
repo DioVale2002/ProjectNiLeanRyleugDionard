3c632902cda8b5fb5a8303d1b0c9428197018788
DioVale2002 <64457128+DioVale2002@users.noreply.github.com>
2025-10-01 09:11:48 +0800

    implement address and customer models in accordance to ERD

---
+++ app/Models/Customer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Customer extends Authenticatable
+{
+    use Notifiable;
+
+    protected $primaryKey = 'cus_id';
+    
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'contact_num',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
+    public function address()
+    {
+        return $this->hasOne(Address::class, 'cus_id', 'cus_id');
+    }
+}
