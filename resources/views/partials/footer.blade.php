b8183cce8033e08af1accb30dea2e71e245163f5
90377Sednaaa <leanmurillo0201@gmail.com>
2026-04-12 19:35:33 +0800

    fix footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="w-screen h-[365px] bg-[#F54E4E] mt-[162px]">
+<footer class="w-screen h-[365px] bg-[#F54E4E] mt-[162px]">
     <div class="flex justify-between items-center px-[124px] pt-[47px]">
         <div class="h-[196px] w-[570px]">
             <img src="{{ asset('images/NCB-LOGO 1.png') }}" alt="Logo" />
@@ -37,22 +37,10 @@
                 © 2025 New Century Books. All rights reserved.
             </p>
         </div>
-        <div class="flex w-[136px] justify-between text-white">
-            <a href="#">
-                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5a4.25 4.25 0 0 0 4.25 4.25h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5a4.25 4.25 0 0 0-4.25-4.25h-8.5zM17.5 6.5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7z" />
-                </svg>
-            </a>
-            <a href="#">
-                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
-                </svg>
-            </a>
-            <a href="#">
-                <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M23.5 6.2a3 3 0 0 0-2.1-2.1C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.4.6A3 3 0 0 0 .5 6.2 31 31 0 0 0 0 12a31 31 0 0 0 .5 5.8 3 3 0 0 0 2.1 2.1c1.9.6 9.4.6 9.4.6s7.5 0 9.4-.6a3 3 0 0 0 2.1-2.1A31 31 0 0 0 24 12a31 31 0 0 0-.5-5.8zM9.6 15.5v-7L16 12l-6.4 3.5z" />
-                </svg>
-            </a>
+        <div class="flex w-[136px] justify-between">
+            <a href="#"><img src="{{ asset('images/Instagram.png') }}" alt="Instagram" /></a>
+            <a href="#"><img src="{{ asset('images/Facebook.png') }}" alt="Facebook" /></a>
+            <a href="#"><img src="{{ asset('images/Youtube.png') }}" alt="Youtube" /></a>
         </div>
     </div>
-</div>
+</footer>
